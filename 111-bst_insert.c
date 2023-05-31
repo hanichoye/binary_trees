8feb23a1250b452decb98d60f477fafa49f3cd95
hanichoye <hana36593@gmail.com>
2023-05-31 02:05:19 -0700

    Advanced Tasks

---
+++ 111-bst_insert.c
@@ -0,0 +1,47 @@
+#include "binary_trees.h"
+
+/**
+* bst_insert - a function that inserts a new node in search tree
+* @tree: The root node
+* @value: new node's value
+* Return: The new node
+*/
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	bst_t *curr, *new;
+
+	if (tree != NULL)
+	{
+		curr = *tree;
+
+		if (curr == NULL)
+		{
+			new = binary_tree_node(curr, value);
+			if (new == NULL)
+				return (NULL);
+			return (*tree = new);
+		}
+
+		if (value < curr->n) /* insert in left subtree */
+		{
+			if (curr->left != NULL)
+				return (bst_insert(&curr->left, value));
+
+			new = binary_tree_node(curr, value);
+			if (new == NULL)
+				return (NULL);
+			return (curr->left = new);
+		}
+		if (value > curr->n) /* insert in right subtree */
+		{
+			if (curr->right != NULL)
+				return (bst_insert(&curr->right, value));
+
+			new = binary_tree_node(curr, value);
+			if (new == NULL)
+				return (NULL);
+			return (curr->right = new);
+		}
+	}
+	return (NULL);
+}
